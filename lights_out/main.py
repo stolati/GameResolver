ccb6e318ff4d6f92c5ac77f8661fafe9d647bc54
Mickael Kerbrat <stolati@gmail.com>
2012-11-21 22:19:44 +0100

    added explanation for sliding

---
+++ lights_out/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#The goal to 
+
+levels = { #line lenght, map, num touch
+  '96': (5, "XX X X XXX  XXX  X    XX ", 14),
+  '92': (5, "  XXX     X    XXXX  X   ", 14),
+  '82': (5, "XX X   XXXX XXX XX XX  X ", 9),
+  '49': (5, "X X X XX  X XXXX   X X   ", 8),
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
